f0f23eac2f3940407b252356a8fe8c3660205789
Usuario <Usuario@CPD02-M01>
2024-03-12 11:58:52 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleAppAposentadoria/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAposentadoria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anos de serviço: ");
+            int anos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Idade do funcionario: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            if ( anos >= 30 || idade >= 65 || idade >= 60 && anos >=25)
+            {
+                Console.WriteLine("Tem direito a aposentadoria");
+            }
+            else 
+            {
+                Console.WriteLine("Não tem direito a aposentadoria");
+            }
+            Console.ReadKey();
+        }
+    }
+}
